e659196882ff03e2aa331ca27b41dd94aaa526ef
kirmir <kirmir@outlook.com>
2014-09-25 11:08:09 +0300

    Added basic controls.

--- ResxTranslationTool/ViewModel/MainViewModel.cs
+++ ResxTranslationTool/ViewModel/MainViewModel.cs
@@ -1,34 +1,23 @@
+using System.Windows;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace ResxTranslationTool.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
         /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            ExitCommand = new RelayCommand(() =>
+                                           {
+                                               Application.Current.Shutdown();
+                                           });
         }
+
+        public ICommand ExitCommand { get; private set; }
     }
 }
